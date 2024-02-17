487aeb300fb985bbb1f0a4193ecdcb16b1527330
Tobi208 <tobi208@github.com>
2024-02-17 09:07:35 +0100

    return authors name instead of userId

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'author']
         read_only_fields = ('author',)
+
+    def get_author(self, obj):
+        return obj.author.username
